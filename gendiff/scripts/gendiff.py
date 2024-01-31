2432b235795d397f28e467701c6a4b0c535829c5
Aleх Bark <thrtth@yandex.ru>
2024-01-31 15:34:27 +0700

    add

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,12 +4,7 @@ from gendiff import generate_diff
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-    parser.add_argument('-f', '--format', type=str, help='set format of output')
-    parser.add_argument('first_file', type=str)
-    parser.add_argument('second_file', type=str)
-    args = parser.parse_args()
-    
+       
     print(generate_diff('file1.json', 'file2.json'))
 
 
