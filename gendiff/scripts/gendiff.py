975bf82153fee798ac939c2012b93bb1e6d916c8
Aleх Bark <thrtth@yandex.ru>
2024-02-06 17:32:10 +0700

    Update gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 import argparse
-from gendiff import generate_diff
 
 
 def main():
-       
-    print(generate_diff('file1.json', 'file2.json'))
+    parser = argparse.ArgumentParser(description='Compares'
+                                                 ' two configuration files'
+                                                 ' and shows a difference.')
+    parser.add_argument('-f', '--format', type=str, help='set format of output')
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    # args = parser.parse_args()
 
 
 if __name__ == '__main__':
