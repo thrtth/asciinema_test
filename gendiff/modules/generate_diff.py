c9c5095c0ff8a0bba1314638eebf608a9a94fb91
Aleх Bark <thrtth@yandex.ru>
2024-02-10 16:27:28 +0700

    add

--- gendiff/modules/generate_diff.py
+++ gendiff/modules/generate_diff.py
@@ -3,11 +3,16 @@ from os import path
 import yaml
 
 
-def bool_to_lowercase(val):
+INDENT = 4
+
+
+def bool_and_none_to_str(val):
     if val is False:
         return 'false'
     elif val is True:
         return 'true'
+    elif val is None:
+        return 'null'
     return val
 
 
@@ -21,24 +26,99 @@ def parse_file(file_path):
     return file_data
 
 
-def generate_diff(file_path1, file_path2):
-    file1 = parse_file(file_path1)
-    file2 = parse_file(file_path2)
-    json_keys = sorted(set(list(file1.keys()) + list(file2.keys())))
-    result_str = '{\n'
-    for key in json_keys:
-        val1 = bool_to_lowercase(file1.get(key))
-        val2 = bool_to_lowercase(file2.get(key))
-        if val1 is not None and val2 is not None:
-            if val1 == val2:
-                result_str += f'    {key}: {val1}\n'
-            else:
-                result_str += f'  - {key}: {val1}\n'
-                result_str += f'  + {key}: {val2}\n'
+def generate_diff(file_path1, file_path2, output_format):
+    formater = globals()[output_format]
+    file_data1 = parse_file(file_path1)
+    file_data2 = parse_file(file_path2)
+    return formater(get_diff(file_data1, file_data2))
+
+
+def is_dict(node):
+    return type(node) is dict
+
+
+def get_diff(dict1, dict2):
+    result = []
+    sorted_keys = sorted(set(list(dict1.keys()) + list(dict2.keys())))
+    for key in sorted_keys:
+        value1 = dict1.get(key)
+        value2 = dict2.get(key)
+        if key not in dict1:
+            result.append({'key': key,
+                           'type': 'added',
+                           'new_value': value2})
+
+        elif key not in dict2:
+            result.append({'key': key,
+                           'type': 'removed',
+                           'old_value': value1})
+
+        elif is_dict(value1) and is_dict(value2):
+            children = get_diff(value1, value2)
+            result.append({'key': key,
+                           'type': 'nested',
+                           'value': children})
+
+        elif value1 == value2:
+            result.append({'key': key,
+                           'type': 'unchanged',
+                           'value': value1})
+
         else:
-            if val2 is None:
-                result_str += f'  - {key}: {val1}\n'
-            else:
-                result_str += f'  + {key}: {val2}\n'
-    result_str += '}'
-    return result_str
+            result.append({'key': key,
+                           'type': 'changed',
+                           'old_value': value1,
+                           'new_value': value2})
+
+    return result
+
+
+def stylish(list_of_dicts, depth=0):
+    result = '{\n'
+    for item in list_of_dicts:
+        item_type = item['type']
+        match item_type:
+            case 'unchanged':
+                value = value_to_str(item["value"], depth)
+                result += f'{" " * depth}    {item["key"]}: {value}\n'
+
+            case 'changed':
+                old_value = value_to_str(item["old_value"], depth)
+                new_value = value_to_str(item["new_value"], depth)
+                result += f'{" " * depth}  - {item["key"]}: {old_value}\n'
+                result += f'{" " * depth}  + {item["key"]}: {new_value}\n'
+
+            case 'added':
+                new_value = value_to_str(item["new_value"], depth)
+                result += f'{" " * depth}  + {item["key"]}: {new_value}\n'
+
+            case 'removed':
+                old_value = value_to_str(item["old_value"], depth)
+                result += f'{" " * depth}  - {item["key"]}: {old_value}\n'
+
+            case 'nested':
+                result += (f'{" " * depth}    {item["key"]}:'
+                           f' {stylish(item["value"], depth + INDENT)}\n')
+
+    result += ' ' * depth + '}'
+    return result
+
+
+def dict_to_str(some_dict, depth):
+    result = '{\n'
+    dict_keys = some_dict.keys()
+    for key in dict_keys:
+        if is_dict(some_dict[key]):
+            result += (f'{" " * depth}    {key}:'
+                       f' {dict_to_str(some_dict[key], depth + INDENT)}\n')
+        else:
+            value = value_to_str(some_dict[key], depth)
+            result += f'{" " * depth}    {key}: {value}\n'
+    result += ' ' * depth + '}'
+    return result
+
+
+def value_to_str(value, depth):
+    if is_dict(value):
+        return dict_to_str(value, depth + INDENT)
+    return bool_and_none_to_str(value)
